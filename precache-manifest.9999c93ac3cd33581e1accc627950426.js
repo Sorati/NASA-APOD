37540bf2cfd4c3e11c4f4b3bc0a4dfb670694f3a
Sorati <egor.dmitrenko.job@gmail.com>
2020-08-05 20:21:15 +0300

    Updates

---
+++ precache-manifest.9999c93ac3cd33581e1accc627950426.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e77d1272e1183ba8a754d375449c6b",
+    "url": "/NASA-APOD/index.html"
+  },
+  {
+    "revision": "7108a156ce8f2bfdd8dd",
+    "url": "/NASA-APOD/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "d6c3a0457c2642dbb313",
+    "url": "/NASA-APOD/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7108a156ce8f2bfdd8dd",
+    "url": "/NASA-APOD/static/js/2.ecb225be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NASA-APOD/static/js/2.ecb225be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6c3a0457c2642dbb313",
+    "url": "/NASA-APOD/static/js/main.f4d4185c.chunk.js"
+  },
+  {
+    "revision": "46474a95185593749b14",
+    "url": "/NASA-APOD/static/js/runtime-main.dd7c509c.js"
+  }
+]);
